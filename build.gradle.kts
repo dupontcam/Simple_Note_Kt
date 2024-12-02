677f3a10c107ca5d47a022047a6a39453b48d660
dupontcam <125046483+dupontcam@users.noreply.github.com>
2024-12-02 12:30:03 -0300

    Activity Main, Colors, Themes

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "br.com.example.simplenote"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.com.example.simplenote"
